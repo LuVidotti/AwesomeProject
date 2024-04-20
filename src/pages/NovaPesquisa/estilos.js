a74916f6393c8a5f884df6e4853ba5b954d6bc2a
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-04-20 03:23:45 -0300

    inserindo tela de nova pesquisa

---
+++ src/pages/NovaPesquisa/estilos.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+    inputArea: {
+        marginHorizontal: 40,
+        marginTop: 40
+    },
+    label: {
+        color: "#FFFFFF",
+        fontSize: 20
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#372775"
+    },
+    inputData: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+        width: 280
+    },
+    input: {
+        backgroundColor: "#FFFFFF",
+        padding: 5,
+        width: 250,
+        color: "#3F92C5"
+    },
+    inputImagem: {
+        backgroundColor: "#FFFFFF",
+        padding: 30,
+        alignItems: "center",
+        marginBottom: 40
+    },
+    inputImagemTexto: {
+        color: "#939393"
+    }
+})
+
+export default estilos;
--- src/pages/NovaPesquisa/index.js
+++ src/pages/NovaPesquisa/index.js
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import Input from "../../components/Input";
+import Icon from "react-native-vector-icons/FontAwesome";
+import estilos from "./estilos";
+import { Button } from "react-native";
+import BotaoPadrao from "../../components/BotaoPadrao";
 
 function NovaPesquisa() {
     return (
-        <View>
-            <Text>Tela de nova pesquisa</Text>
+        <View style={estilos.container}>
+            <View style={estilos.inputArea}>
+                <Text style={estilos.label}>Nome</Text>
+                <Input isPassword={false}/>
+            </View>
+
+            <View style={estilos.inputArea}>
+                <Text style={estilos.label}>Data</Text>
+                <View style={estilos.inputData}>
+                    <TextInput style={estilos.input}/>
+                    <Icon name="calendar" style={{color: "#939393", backgroundColor: "#FFFFFF", fontSize: 20}}/>
+                </View>
+            </View>
+
+            <View style={estilos.inputArea}>
+                <Text style={estilos.label}>Imagem</Text>
+                <TouchableOpacity style={estilos.inputImagem}>
+                    <Text style={estilos.inputImagemTexto}>Câmera/Galeria de imagens</Text>
+                </TouchableOpacity>
+            </View> 
+
+            <BotaoPadrao texto="CADASTRAR"/>
         </View>
     )
 }
