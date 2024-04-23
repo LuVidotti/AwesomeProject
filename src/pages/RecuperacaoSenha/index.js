d6e2dd2e4439864ed60adcfd39fc9f56feb1608d
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-04-23 01:11:01 -0300

    inserindo validacao de email na tela recuperacao de senha

--- src/pages/RecuperacaoSenha/index.js
+++ src/pages/RecuperacaoSenha/index.js
@@ -2,17 +2,36 @@ import { Text, View } from "react-native";
 import estilos from "./estilos";
 import Input from "../../components/Input";
 import BotaoPadrao from "../../components/BotaoPadrao";
+import { useState } from "react";
+import MensagemErro from "../../components/MensagemErro";
 
-function RecuperacaoSenha() {
+function RecuperacaoSenha({ navigation }) {
+    const [email, setEmail] = useState("");
+    const [mensagemErro, setMensagemErro] = useState('');
+
+    function recuperarSenha() {
+        const reEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        if(reEmail.test(email)) {
+            setMensagemErro('');
+            navigation.navigate("Login");
+        } else {
+            setMensagemErro("E-mail parece ser inválido");
+        }
+    }
+
     return (
         <View style={estilos.container}>
             <View style={estilos.inputArea}>
                 <Text style={estilos.label}>E-mail</Text>
-                <Input isPassword={false}/>
+                <Input valor={email} acao={setEmail} isPassword={false}/>
+                {
+                    mensagemErro !== "" ? <MensagemErro texto={mensagemErro}/> : null
+                }
             </View>
 
             <View style={{marginTop: 50}}>
-                <BotaoPadrao texto="RECUPERAR"/>
+                <BotaoPadrao acao={recuperarSenha} texto="RECUPERAR"/>
             </View>
         </View>
     )
