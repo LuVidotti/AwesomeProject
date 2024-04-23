ec8da7d080a85c34b3f9fbe42155bb498ae9a71a
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-04-23 01:51:24 -0300

    inserindo validacao de campos na tela nova pesquisa

--- src/pages/NovaPesquisa/index.js
+++ src/pages/NovaPesquisa/index.js
@@ -3,21 +3,46 @@ import Input from "../../components/Input";
 import Icon from "react-native-vector-icons/FontAwesome";
 import estilos from "./estilos";
 import BotaoPadrao from "../../components/BotaoPadrao";
+import { useState } from "react";
+import MensagemErro from "../../components/MensagemErro";
 
-function NovaPesquisa() {
+function NovaPesquisa({ navigation }) {
+    const [nome, setNome] = useState('');
+    const [data, setData] = useState('');
+    const [mensagemErroNome, setMensagemErroNome] = useState('');
+    const [mensagemErroData, setMensagemErroData] = useState("");
+
+    function cadastrar() {
+        if(nome === "" || data === "") {
+            setMensagemErroData("Preencha a data");
+            setMensagemErroNome("Preencha no nome da pesquisa");
+            return
+        }
+
+        setMensagemErroData("");
+        setMensagemErroNome("");
+        navigation.goBack();
+    }
+
     return (
         <View style={estilos.container}>
             <View style={estilos.inputArea}>
                 <Text style={estilos.label}>Nome</Text>
-                <Input isPassword={false}/>
+                <Input valor={nome} acao={setNome} isPassword={false}/>
+                {
+                    mensagemErroNome !== "" ? <MensagemErro texto={mensagemErroNome}/> : null
+                }
             </View>
 
             <View style={estilos.inputArea}>
                 <Text style={estilos.label}>Data</Text>
                 <View style={estilos.inputData}>
-                    <TextInput style={estilos.input}/>
+                    <TextInput value={data} onChangeText={setData} style={estilos.input}/>
                     <Icon name="calendar" style={{color: "#939393", backgroundColor: "#FFFFFF", fontSize: 20}}/>
                 </View>
+                {
+                    mensagemErroData !== "" ? <MensagemErro texto={mensagemErroData}/> : null
+                }
             </View>
 
             <View style={estilos.inputArea}>
@@ -27,7 +52,7 @@ function NovaPesquisa() {
                 </TouchableOpacity>
             </View> 
 
-            <BotaoPadrao texto="CADASTRAR"/>
+            <BotaoPadrao acao={cadastrar} texto="CADASTRAR"/>
         </View>
     )
 }
